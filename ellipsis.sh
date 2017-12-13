dc5d0fa330a6cdff0a9c8886f643a07f356c66c0
Adam Bergman <adam@fransvilhelm.com>
2017-12-13 11:10:29 +0100

    :wrench: Update ellipsis script

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,24 +1,28 @@
-#!/usr/bin/env bash
-#
-# adambrgmn/dotfiles ellipsis package
+# !/usr/bin/env bash
+# adambrgmn/dotfiles - ellipsis package
+ELLIPSIS_VERSION_DEP='1.9.4'
 
-# The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
+pkg.install() {
+  if [ $(npm ls -g --depth=0 | grep 'pure-prompt') ]; then
+    log.fail "'pure-prompt' must be installed ('npm i -g pure-prompt')"
+    return 1
+  fi
+}
 
-# pkg.push() {
-#     git.push
-# }
+pkg.push() {
+  git.push
+}
 
-# pkg.pull() {
-#     git.pull
-# }
+pkg.pull() {
+  # Use improved update strategy
+  git remote update 2>&1 > /dev/null
+  if git.is_behind; then
+    pkg.unlink
+    git.pull
+    pkg.link
+  fi
+}
 
-# pkg.installed() {
-#     git.status
-# }
-#
-# pkg.status() {
-#     git.diffstat
-# }
+pkg.status() {
+  git.diffstat
+}
