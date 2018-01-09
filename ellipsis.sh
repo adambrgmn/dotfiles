e780795c832715d69d5dc6ae662f48e936b5e3e1
Adam Bergman <adam@fransvilhelm.com>
2018-01-09 16:35:01 +0100

    Update ellipsis scripts

--- ellipsis.sh
+++ ellipsis.sh
@@ -2,6 +2,8 @@
 # adambrgmn/dotfiles - ellipsis package
 ELLIPSIS_VERSION_DEP='1.9.4'
 
+DIRS=(".iTerm2")
+
 pkg.install() {
   if [ $(npm ls -g --depth=0 | grep 'pure-prompt') ]; then
     log.fail "'pure-prompt' must be installed ('npm i -g pure-prompt')"
@@ -14,10 +16,19 @@ pkg.push() {
 }
 
 pkg.link() {
-  mkdir -p "${ELLIPSIS_HOME}/.iTerm2"
-  fs.link_file "${PKG_PATH}/.iTerm2/com.googlecode.iTerm2.plist" "${ELLIPSIS_HOME}/.iTerm2/com.googlecode.iterm2.plist"
-  
-  fs.link_files $PKG_PATH
+  for dir in ${DIRS[@]}; do
+    fs.link_file "${PKG_PATH}/${dir}" "${ELLIPSIS_HOME}/${dir}"
+  done
+
+  hooks.link
+}
+
+pkg.unlink() {
+  for dir in ${DIRS[@]}; do
+    rm "${ELLIPSIS_HOME}/${dir}"
+  done
+
+  hooks.unlink
 }
 
 pkg.pull() {
