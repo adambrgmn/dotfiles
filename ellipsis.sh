c120d23c609934db26a9b09e83bada2d23e6580f
Adam Bergman <adam@fransvilhelm.com>
2018-01-26 11:44:21 +0100

    Enhance install and pull hooks
    Install hook will now correctly check for dependecies (npm and zsh plugins) and download them if they aren't present.
    
    Pull hook will also update the dependencies.

--- ellipsis.sh
+++ ellipsis.sh
@@ -2,13 +2,32 @@
 # adambrgmn/dotfiles - ellipsis package
 ELLIPSIS_VERSION_DEP='1.9.4'
 
+ZSH_PLUGINS="${ZSH}/custom/plugins"
 DIRS=(".iTerm2")
 
+ZSH_DEPS=(zsh-autosuggestions zsh-syntax-highlighting)
+NPM_DEPS=(pure-prompt)
+
 pkg.install() {
-  if [ $(npm ls -g --depth=0 | grep 'pure-prompt') ]; then
-    log.fail "'pure-prompt' must be installed ('npm i -g pure-prompt')"
-    return 1
-  fi
+  # Install npm dependencies
+  for plugin in ${NPM_DEPS[@]}; do
+    if npm ls -g --depth=0 | grep $plugin; then
+      msg.bold "Skip installing $plugin"
+    else
+      msg.bold "Installing $plugin"
+      npm i -g $plugin --silent
+    fi
+  done
+
+  # Install zsh plugins
+  for plugin in ${ZSH_DEPS[@]}; do
+    if [ -d "${ZSH_PLUGINS}/$plugin" ]; then
+      msg.bold "Skip installing $plugin"
+    else
+      msg.bold "Installing $plugin"
+      git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PLUGINS/$plugin --quiet
+    fi
+  done
 }
 
 pkg.push() {
@@ -32,6 +51,8 @@ pkg.unlink() {
 }
 
 pkg.pull() {
+  msg.bold "updating dotfiles"
+
   # Use improved update strategy
   git remote update 2>&1 > /dev/null
   if git.is_behind; then
@@ -39,6 +60,26 @@ pkg.pull() {
     git.pull
     pkg.link
   fi
+
+  # Update dependencies
+  for plugin in ${NPM_DEPS[@]}; do
+    msg.dim $(msg.tabs) "updating $plugin"
+    npm i -g $plugin --silent
+  done
+
+  for plugin in ${ZSH_DEPS[@]}; do
+    cd "$ZSH_PLUGINS/$plugin"
+
+    git remote update 2>&1 > /dev/null
+    if git.is_behind; then
+      msg.dim "updating dependency $plugin"
+      git pull
+    else
+      msg.dim "skip updating $plugin"
+    fi
+  done
+
+  cd $PKG_PATH
 }
 
 pkg.status() {
